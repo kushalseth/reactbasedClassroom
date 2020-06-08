24fe414334ac15a42d7b692aa6615d9cb9f1f8e4
Kushal Seth <kushalseth1408@gmail.com>
2020-06-09 00:53:26 +0530

    react project setup

---
+++ reactapp/application/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+function App() {
+    return <p>Hello React</p>
+}
+
+render(<App></App>, document.getElementById("app"));
---
+++ reactapp/application/webpack.config.dev.js
@@ -0,0 +1,48 @@
+const webpact = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+    mode: "development",
+    target: "web",
+    // source map help us see original code in browser
+    devtool: "cheap-module-source-map",
+    entry: "./src/index",
+    // physical file won't be generated for development.
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    // we can chose any node based web server as well like express
+    devServer: {
+        stats: "minimal",
+        overlay: true,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        https: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"]
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+
+
+}
