be0c6a35ce26b9b96f303f8b76a08150c70fbfda
Kushal Seth <kushalseth1408@gmail.com>
2020-06-11 02:31:36 +0530

    added author reducer

--- reactapp/application/src/redux/reducers/courseReducer.js
+++ reactapp/application/src/redux/reducers/courseReducer.js
@@ -1,9 +1,12 @@
 import * as types from "../actions/actionTypes";
 
 export default function courseReducer(state = [], action) {
+  debugger;
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state, { ...action.course }];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
